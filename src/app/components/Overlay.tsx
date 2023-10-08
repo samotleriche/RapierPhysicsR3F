da9f1c704fbbbe78210d4fe183bb36bda87952ce
samotleriche <samotleriche@gmail.com>
2023-10-08 12:09:38 -0700

    fix menu

--- src/app/components/Overlay.tsx
+++ src/app/components/Overlay.tsx
@@ -17,14 +17,14 @@ const Overlay = ({
       <button
         onClick={() => setToggleOpen(!toggleOpen)}
         className={
-          "bg-indigo-700 z-20 px-5 py-2 rounded-md absolute top-6 right-6"
+          "bg-indigo-700 z-20 px-5 py-2 rounded-md absolute top-3 right-0"
         }
       >
         {toggleOpen ? "close" : "open"}
       </button>
       <div
         className={clsx(
-          "rounded-md absolute p-5 z-10 mb-8 transition-all ease-in-out duration-300 top-14 right-6 flex flex-col items-center justify-center group bg-indigo-700  w-auto",
+          "rounded-md absolute p-5 z-10 mb-8 transition-all ease-in-out duration-300 top-12 right-1 flex flex-col items-center justify-center group bg-indigo-700  w-auto",
           toggleOpen ? "h-auto" : "translate-x-full"
         )}
       >
@@ -51,12 +51,12 @@ const Overlay = ({
         >
           See Walls
         </button>
-        <p>max = 700</p>
+        <p>max cubes = 700</p>
         <input
           className="w-full rounded-md border-0 py-2 px-3 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
           type="number"
           placeholder={cubeCount}
-          value={isNaN(cubeCount) ? 0 : cubeCount}
+          value={cubeCount}
           onChange={(e) => {
             if (parseInt(e.target.value) > 700) {
               setCubeCount(700);
